44c45167bee948def38fdb89ad2d8a5f6e2213db
Austin Matthews <austm14@gmail.com>
2024-03-20 14:56:59 -0400

    Moved api calls from the ui into agent.ts; Added error testing page to about page

--- UI/src/features/catalog/Catalog.tsx
+++ UI/src/features/catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import agent from '../../app/api/agent'
 import { Product } from '../../app/layout/models/products'
 import ProductList from './ProductList'
 import { useEffect, useState } from 'react'
@@ -6,14 +7,12 @@ export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([])
   useEffect(
     () => {
-      fetch('http://localhost:5000/api/products')
-        .then((response) => response.json())
-        .then((data) => setProducts(data))
+      agent.Catalog.list().then((products) => setProducts(products))
     },
     // This second parameter below (The empty array) is an array on dependencies
     // An Empty array will make it only run once
     //If you don't add this, it will rerender infinitely
-    [],
+    []
   )
 
   return (
