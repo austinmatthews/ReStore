8a9e19dd3f30e68b21d8896f0b7457d9c8dd8dd4
Austin Matthews <austm14@gmail.com>
2024-03-25 17:27:14 -0400

    75: Updating the header with the basket item count

--- UI/src/app/layout/Header.tsx
+++ UI/src/app/layout/Header.tsx
@@ -11,6 +11,7 @@ import {
   Typography,
 } from '@mui/material'
 import { Link, NavLink } from 'react-router-dom'
+import { useStoreContext } from '../context/StoreContext'
 
 const midLinks = [
   { title: 'catalog', path: '/catalog' },
@@ -37,6 +38,12 @@ interface Props {
 }
 
 export default function Header({ darkMode, onSwitch }: Props) {
+  const { basket } = useStoreContext()
+
+  const itemCount = basket?.items.reduce((sum, item) => {
+    return sum + item.quantity
+  }, 0)
+
   return (
     <>
       <AppBar position="static" sx={{ mb: 4 }}>
@@ -75,7 +82,7 @@ export default function Header({ darkMode, onSwitch }: Props) {
               color="inherit"
               sx={{ mr: 2 }}
             >
-              <Badge badgeContent="4" color="secondary">
+              <Badge badgeContent={itemCount} color="secondary">
                 <ShoppingCart />
               </Badge>
             </IconButton>
