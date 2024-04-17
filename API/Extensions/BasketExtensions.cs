945e7e751ccc6213dac30649697eaf8e280156cc
Austin Matthews <austm14@gmail.com>
2024-04-17 13:27:10 -0400

    152. Adding the create order method

--- API/Extensions/BasketExtensions.cs
+++ API/Extensions/BasketExtensions.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
 {
@@ -24,6 +25,17 @@ namespace API.Extensions
 					})
 					.ToList()
 			};
+		}
+
+		public static IQueryable<Basket> RetrieveBasketWithItems(
+			this IQueryable<Basket> query,
+			string buyerId
+		)
+		{
+			return query
+				.Include(i => i.Items)
+				.ThenInclude(p => p.Product)
+				.Where(b => b.BuyerId == buyerId);
 		}
 	}
 }
